d30ec38f56985057caab1d8afd63b640bc2a948e
0403_itou <61015756+itopon1999@users.noreply.github.com>
2020-09-13 01:47:05 +0900

    Add files via upload

---
+++ MemberData/0403/Audio/AudioBGM.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioBGM : MonoBehaviour
+{
+    public AudioSource[] Source;                 //オーディオソース
+    public int SoundNum;                        //音源番号
+
+    void Start()
+    {
+        Source[SoundNum] = GetComponent<AudioSource>();
+    }
+
+    public void AudioBGMSet(int SoundNum, float Vol)//BGM再生用関数(再生したい音源番号,ボリューム 0.0f～1.0f)
+    {
+        Source[SoundNum].Play();      //音源再生
+        Source[SoundNum].volume = Vol;                      //ボリューム
+    }
+    public void AudioBGMUnLoop()                    //BGMループ解除関数
+    {
+        Source[SoundNum].loop = Source[SoundNum].loop = false;
+    }
+}
+/*
+　 実装方法
+  このスクリプトは空objectにつけてください。
+
+  AudioBGMObj = GameObject.FindGameObjectWithTag("BGMManager");//BGMManager検索
+  上記をStart設定し、音源を生成したい場所に
+  AudioBGMObj.GetComponent<AudioBGM>().AudioBGMSet(1, 1.0f);
+*/
---
+++ MemberData/0403/Audio/AudioSE.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSE : MonoBehaviour
+{
+    public AudioClip[] Sound;                   //音源配列
+    private AudioSource Source;                 //オーディオソース
+    public int SoundNum;                        //音源番号
+
+    void Start()
+    {
+        Source = GetComponent<AudioSource>();
+    }
+
+    public void AudioSESet(int SoundNum, float Vol)//サウンドエフェクト用関数(再生したい音源番号,ボリューム 0.0f～1.0f)
+    {
+        Source.PlayOneShot(Sound[SoundNum]);      //音源再生
+        Source.volume = Vol;                      //ボリューム
+    }
+}
+/*
+　 実装方法
+  このスクリプトは空objectにつけてください。
+
+  AudioSEObj = GameObject.FindGameObjectWithTag("SEManager");//SEManager検索
+  上記をStart設定し、音源を生成したい場所に
+  AudioSEObj.GetComponent<AudioSE>().AudioSESet(1, 1.0f);
+*/
